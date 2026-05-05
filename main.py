24ef2b328b9434f98cb0f26556d668c0b3814a74
Jorge Castillo <skmrubik@gmail.com>
2026-05-05 16:21:11 +0200

    dibujando el camino encontrado por el algoritmo

--- main.py
+++ main.py
@@ -36,6 +36,19 @@ def main():
                     url = "http://localhost:8080/ciudades"
 
                     response = requests.get(url, params={"ciudades": ciudades})
+                    print(response.json())
+                    secuencia = response.json()
+                    coordenadas_inicio = ciudades[secuencia[0]]
+                    coordenadas_final = ciudades[secuencia[len(secuencia)-1]]
+                    pygame.draw.line(WIN, (0, 255, 0), (coordenadas_inicio[0], coordenadas_inicio[1]), (coordenadas_final[0], coordenadas_final[1]), 2)
+                    for i, ciudad in enumerate(secuencia):
+                        if i < len(secuencia)-1:
+                            coordenadas_uno = ciudades[secuencia[i]]
+                            coordenadas_dos = ciudades[secuencia[i+1]]
+                            pygame.draw.line(WIN, (0, 255, 0), (coordenadas_uno[0], coordenadas_uno[1]), (coordenadas_dos[0], coordenadas_dos[1]), 2)
+                    #for i, ciudad in enumerate(ciudades):
+                        #pygame.draw.circle(WIN, (255, 0, 0), (ciudad[0], ciudad[1]), 5)
+                    pygame.display.update()
 
     pygame.quit()
 
