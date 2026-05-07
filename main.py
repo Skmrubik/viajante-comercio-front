064d8d59bc2af68b75655ea4f37f9dd1cc12e75a
Jorge Castillo <skmrubik@gmail.com>
2026-05-07 16:01:55 +0200

    imagenes actualizadas

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ def main():
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
                 ciudades.append([row,col])
-                pygame.draw.circle(WIN, (255, 0, 0), (row, col), 5)
+                pygame.draw.circle(WIN, (255, 255, 255), (row, col), 12)
+                pygame.draw.circle(WIN, (255, 0, 0), (row, col), 10)
                 pygame.display.update()
                 print(ciudades)
             if event.type == pygame.KEYDOWN:
@@ -40,15 +41,16 @@ def main():
                     secuencia = response.json()
                     coordenadas_inicio = ciudades[secuencia[0]]
                     coordenadas_final = ciudades[secuencia[len(secuencia)-1]]
-                    pygame.draw.line(WIN, (255, 255, 255), (coordenadas_inicio[0], coordenadas_inicio[1]), (coordenadas_final[0], coordenadas_final[1]), 2)
+                    pygame.draw.line(WIN, (255, 255, 255), (coordenadas_inicio[0], coordenadas_inicio[1]), (coordenadas_final[0], coordenadas_final[1]), 9)
                     for i, ciudad in enumerate(secuencia):
                         if i < len(secuencia)-1:
                             coordenadas_uno = ciudades[secuencia[i]]
                             coordenadas_dos = ciudades[secuencia[i+1]]
                             print(str(coordenadas_uno)+' '+str(coordenadas_dos))
-                            pygame.draw.line(WIN, (255, 255, 255), (coordenadas_uno[0], coordenadas_uno[1]), (coordenadas_dos[0], coordenadas_dos[1]), 2)
-                    #for i, ciudad in enumerate(ciudades):
-                        #pygame.draw.circle(WIN, (255, 0, 0), (ciudad[0], ciudad[1]), 5)
+                            pygame.draw.line(WIN, (255, 255, 255), (coordenadas_uno[0], coordenadas_uno[1]), (coordenadas_dos[0], coordenadas_dos[1]), 9)
+                    for i, ciudad in enumerate(ciudades):
+                        pygame.draw.circle(WIN, (255, 255, 255), (ciudad[0], ciudad[1]), 12)
+                        pygame.draw.circle(WIN, (255, 0, 0), (ciudad[0], ciudad[1]), 10)
                     pygame.display.update()
 
     pygame.quit()
